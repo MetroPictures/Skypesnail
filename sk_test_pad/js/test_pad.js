53a891c7c35b8eacb9f1cabf8c362010afb5040b
harlo <harlo.holmes@gmail.com>
2015-10-17 19:08:42 +0000

    with task pad

---
+++ sk_test_pad/js/test_pad.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	var hup = "hang_up";
+	var pup = "pick_up";
+
+	$("#mp_receiver")
+		.html(pup)
+		.click(function() {
+			if($(this).html() == hup) {
+				next_state = pup;
+			} else if($(this).html() == pup) {
+				next_state = hup;
+			}
+
+			$.ajax({
+				url : $(this).html(),
+				context : this
+			}).done(function(json) {
+				console.info(json);				
+				$(this).html(next_state);
+			});
+		});
+
+	$("#mp_only_press")
+		.click(function() {
+			$.ajax({
+				url : "/mapping/3"
+			}).done(function(json)) {
+				console.info(json);
+			}
+		});
+});
