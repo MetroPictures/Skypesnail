01d21ca0ff2db3a49f72b51e91b94614cd96c87b
harlo <harlo.holmes@gmail.com>
2015-11-02 12:38:27 +0000

    much simpler

--- sk_test_pad/js/test_pad.js
+++ sk_test_pad/js/test_pad.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
-	var hup = "hang_up";
-	var pup = "pick_up";
-
-	$("#mp_receiver")
-		.html(pup)
-		.click(function() {
-			if($(this).html() == hup) {
-				next_state = pup;
-			} else if($(this).html() == pup) {
-				next_state = hup;
-			}
-
-			$.ajax({
-				url : $(this).html(),
-				context : this
-			}).done(function(json) {
-				console.info(json);				
-				$(this).html(next_state);
-			});
-		});
-
 	$("#mp_only_press")
 		.click(function() {
 			$.ajax({
--- skypesnail.py
+++ skypesnail.py
@@ -4,8 +4,14 @@ from time import sleep, time
 
 from core.vars import BASE_DIR
 from core.api import MPServerAPI
-from core.utils import get_config, millis_to_time_str
+from core.utils import get_config, micros_to_time_str
 from core.video_pad import MPVideoPad
+
+# time in micros
+
+DAD_VID = 0
+KID_VID = 1
+VID_DIFFERENTIAL = 248920000
 
 class Skypesnail(MPServerAPI, MPVideoPad):
 	def __init__(self):
@@ -20,58 +26,20 @@ class Skypesnail(MPServerAPI, MPVideoPad):
 			}
 		})
 
-		self.dad_video = "SKYPESNAIL_1.mp4"
-		self.kid_video = "SKYPESNAIL_2.mp4"
-
 		MPVideoPad.__init__(self)
 		logging.basicConfig(filename=self.conf['d_files']['module']['log'], level=logging.DEBUG)
 
 	def start_skypesnail(self):
 		logging.debug("Starting the whole thing")
 
-		global_timekeeper = {
-			'start_time' : 0,
-			'last_pause_time' : 0,
-			'position_at_last_pause' : 0,
-			'master_video_index' : -1
-		}
+		self.db.set("current_video", DAD_VID)
 
-		self.db.set('global_timekeeper', json.dumps(global_timekeeper))
-		self.db.set("current_video", self.dad_video)
-
-		return self.play_video(self.dad_video, video_callback=self.video_listener_callback)	
+		return self.play_video("SKYPESNAIL_MERGED.mp4", \
+			with_extras={'loop' : ""}, \
+			video_callback=self.video_listener_callback)	
 
 	def video_listener_callback(self, info):
-		global_timekeeper = json.loads(self.db.get('global_timekeeper'))
-		
-		# set the first timekeeper!
-		if global_timekeeper['start_time'] == 0 and 'start_time' in info['info'].keys():
-			global_timekeeper['start_time'] = info['info']['start_time']
-			global_timekeeper['master_video_index'] = info['index']
-
-		# TODO: take account for positioning...
-		if 'with_extras' in info['info'].keys() and 'pos' in info['info']['with_extras'].keys():
-			print "VID HAS POSITION EXTRAS!"
-			global_timekeeper['position_debounce'] = info['info']['with_extras']['pos']
-			print global_timekeeper['position_debounce']
-			# x seconds FROM where i started!
-
-		# of if paused...
-		if 'position_at_last_pause' in info['info'].keys():
-			old_position = global_timekeeper['position_at_last_pause']
-			new_position = (old_position + abs(info['info']['last_pause_time'] - global_timekeeper['start_time']))
-
-			global_timekeeper['position_at_last_pause'] = new_position
-			global_timekeeper['last_pause_time'] = info['info']['last_pause_time']
-
-		# and finally stopped...
-		if 'stopped' in info['info'].keys():
-			# start video in next position at pause time from current video
-			self.play_video(video=self.db.get('current_video'), \
-				with_extras={'pos' : millis_to_time_str(global_timekeeper['position_at_last_pause'] * 1000)}, \
-				video_callback=self.video_listener_callback)
-
-		self.db.set('global_timekeeper', json.dumps(global_timekeeper))
+		print info
 
 		try:
 			video_info = self.get_video_info(info['index'])
@@ -87,18 +55,22 @@ class Skypesnail(MPServerAPI, MPVideoPad):
 
 	def toggle_placement(self):
 		try:
-			global_timekeeper = json.loads(self.db.get('global_timekeeper'))
-
 			# get video in current position
-			current_video = self.db.get("current_video")
-			next_video = self.kid_video if current_video == self.dad_video else self.dad_video
-			
-			# pause it
-			self.pause_video(video=current_video, video_callback=self.video_listener_callback)
+			current_video = int(self.db.get("current_video"))
+			next_video = KID_VID if current_video == DAD_VID else DAD_VID
 
-			# stop it
-			self.stop_video(video=current_video, video_callback=self.video_listener_callback)
+			# get current position
+			current_position = self.get_video_position()
+			if current_position is None:
+				return False
 			
+			# set new position
+			new_position = (current_position + VID_DIFFERENTIAL) if current_video == DAD_VID \
+				else (current_position - VID_DIFFERENTIAL)
+
+			logging.debug("Seeking to video %d (%s)" % (next_video, "DAD" if next_video == DAD_VID else "KID"))			
+			self.set_video_position(new_position, video_callback=self.video_listener_callback)
+
 			# update db
 			self.db.set('current_video', next_video)
 
@@ -121,21 +93,15 @@ class Skypesnail(MPServerAPI, MPVideoPad):
 			return False
 
 		# auto-start!
+		sleep(3)
 		try:
 			r = requests.get("http://localhost:%d/pick_up" % self.conf['api_port'])
-			print r.content
-			return True
+			return json.loads(r.content)['ok']
 		except Exception as e:
 			logging.error("Could not start?")
 			print e, type(e)
 
 		return False
-
-
-	def reset_for_call(self):
-		self.db.delete("global_timekeeper")
-
-		super(Skypesnail, self).reset_for_call()
 
 	def on_hang_up(self):
 		self.stop_video_pad()
@@ -157,3 +123,4 @@ if __name__ == "__main__":
 		res = ss.start()
 
 	exit(0 if res else -1)
+
